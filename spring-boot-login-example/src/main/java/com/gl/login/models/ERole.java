fcc5874d580f08847cb0cb289ea2a2c6b18531ae
ratnappatel <ratnappatel@gmail.com>
2023-02-02 09:59:47 +0530

    Spring Boot Role based JWT

---
+++ spring-boot-login-example/src/main/java/com/gl/login/models/ERole.java
@@ -0,0 +1,6 @@
+package com.gl.login.models;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_ADMIN
+}
---
+++ spring-boot-login-example/src/main/java/com/gl/login/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.gl.login.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gl.login.models.ERole;
+import com.gl.login.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
